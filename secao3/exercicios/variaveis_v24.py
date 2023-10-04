03bbc7af1667219e3c06874c43818cf0c39a6880
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-10-04 11:05:44 -0300

    Create variaveis_v24.py

---
+++ secao3/exercicios/variaveis_v24.py
@@ -0,0 +1,10 @@
+nome = 'pedro'
+sobrenome = 'damiao'
+idade = 18
+altura = 1.80
+maior_idade = idade >= 18
+print("Seu nome:", nome)
+print("Seu sobrenome:", sobrenome)
+print("Sua idade:", idade)
+print("Sua altura:", altura)
+print("É maior de idade:", "Sim" if maior_idade else "não")
