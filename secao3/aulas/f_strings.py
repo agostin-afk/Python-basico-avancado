4298d6a80a0c2f9cd65f80c8513cb62282614d50
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-10-04 15:20:17 -0300

    update

---
+++ secao3/aulas/f_strings.py
@@ -0,0 +1,3 @@
+valor_banco = 10000000000
+valor_formatado = f'{valor_banco:,.2f}'
+print(valor_formatado)
---
+++ secao3/exercicios/calculo_imc.py
@@ -0,0 +1,5 @@
+peso = 71.0
+altura = 1.83
+imc = peso/altura**2
+print("seu imc é: ", imc)
+print(...)
