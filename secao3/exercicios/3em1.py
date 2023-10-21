40a1a74605022ce722395709afd5f3065766275b
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-10-21 13:05:40 -0300

    Update 3em1.py

--- secao3/exercicios/3em1.py
+++ secao3/exercicios/3em1.py
@@ -3,20 +3,20 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-""" try:
+try:
     num_int= int(input('informe um numero inteiro: '))
     if num_int%2 == 0:
         print('esse numero é divisivel por 2')
     else: 
         print('esse numero não é divisivel por 2')
 except:
-    print('O numero precisa ser inteiro, tente novamente.') """
+    print('O numero precisa ser inteiro, tente novamente.')
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-""" try:
+try:
     hora = int(input('informe a hora do atual momento: '))
     if hora in range(0,12):
         print('Bom dia!!!')
@@ -28,7 +28,7 @@ Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
         print('Hora errada')
     
 except:
-    print('erro, tente novamente') """
+    print('erro, tente novamente')
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
