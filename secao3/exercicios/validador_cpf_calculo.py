c6eaa228b92703176ab76326b5e559b717e6f4ea
Agostinho Ferreira <agostinho008.af45@gmail.com>
2023-11-30 14:33:57 -0300

    Update validador_cpf_calculo.py

--- secao3/exercicios/validador_cpf_calculo.py
+++ secao3/exercicios/validador_cpf_calculo.py
@@ -1,9 +1,8 @@
 import re
-multiplicacao = 11
+multiplicacao = 10
 soma = 0
 #cpf = input('informe seu cpf: ')
 cpf = re.sub(r'[^0-9]', '', input('informe seu cpf: '))
-#print(cpf)
 entrada = cpf
 teste_sequencia = entrada != cpf[0] * len(cpf)
 if teste_sequencia:
@@ -13,19 +12,20 @@ if teste_sequencia:
         soma += mult_valor
         multiplicacao -= 1
         if i == 9:
-            penultimo_digito = int(k)
+            penultimo_digito = k
+            soma_digi_1 = soma * 10
+            soma_digi_1 %= 11
+            resultado_digi_1 = soma_digi_1 if soma_digi_1 <= 9 else 0
+        if i == 10:
+            ultimo_digito = k
+            soma_digi_2 = soma * 10
+            soma_digi_2 %= 11
+            resultado_digi_2 = soma_digi_2 if soma_digi_2 <= 9 else 0
+        #print(resultado_digi_1)
+    print(resultado_digi_1)
+        
+    # print(cpf[9:])
+    
+    
 else:
     print('você digitou o mesmo numero em sequência, tente novamente!')
-
-
-
-""" mult_numero = 10
-for i,k in enumerate(cpf[:11]):
-    
-    if k in '.':
-        pass
-    else:
-        mult_valor = int(k) * mult_numero
-        soma += mult_valor
-        mult_numero -= 1   
- """
