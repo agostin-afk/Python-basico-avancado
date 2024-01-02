7badbaa91ed7a5a8b46a70fd93db1e0e2d7ef3eb
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-01-01 22:10:27 -0300

    Update relacao_entre_classes.py

--- secao5/exercicios/relacao_entre_classes.py
+++ secao5/exercicios/relacao_entre_classes.py
@@ -7,12 +7,17 @@ class Carro:
         
     @property
     def motor(self):
-     property
+        return self._motor
     @motor.setter
     def motor(self, value):
         self._motor = value
         
     @property
+    def faricante(self):
+        return self._fabricante
+    @faricante.setter
+    def fabricante(self, value):
+        self._fabricante = value
 class Motor:
     
     def __init__(self, nome):
@@ -24,7 +29,10 @@ class Fabricante:
         self.nome = nome
         
 carro1 = Carro('gol')
-motor1 = Motor('v8')
+motor1 = Motor('1.0')
+fabricante1 = Fabricante('Volkswagen')
+carro1.fabricante = fabricante1
 carro1.motor = motor1
 
-print(carro1.motor)
+print(vars(carro1), sep='\n')
+print(f'{carro1.nome}\n{carro1.fabricante.nome}\n{carro1.motor.nome}')
