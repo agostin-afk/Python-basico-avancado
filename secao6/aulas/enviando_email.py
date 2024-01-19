fb17a4bcb9e0b7e13add4c2fde5b08d08d5d1ecc
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-01-19 00:01:56 -0300

    envio de email personalizado

--- secao6/aulas/enviando_email.py
+++ secao6/aulas/enviando_email.py
@@ -1,7 +1,10 @@
 import os
 import pathlib
+from string_ import MyTemplate
 from dotenv import load_dotenv
-
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
 
 load_dotenv()
@@ -19,5 +22,22 @@ smtp_port = os.getenv('smtp_port', '')
 smtp_username = os.getenv('smtp_username', '')
 smtp_password = os.getenv('smtp_password', '')
 
-with open(CAMINHO_HTML, 'r') as arquivo:
+with open(CAMINHO_HTML, 'r', encoding='utf8') as arquivo:
     texto_arquivo = arquivo.read()
+    template = MyTemplate(texto_arquivo)
+    texto_email = template.substitute(nome ='Agosto', escritor='Agosto')
+
+mime_multpart = MIMEMultipart()
+mime_multpart['from'] = remetente
+mime_multpart['to'] = destinatario
+mime_multpart['subject'] = 'Este é o assunto do Email'
+
+corpo_email = MIMEText(texto_email, 'html', 'utf-8')
+mime_multpart.attach(corpo_email)
+
+with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.ehlo()
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.send_message(mime_multpart)
+    print('Email enviado')
