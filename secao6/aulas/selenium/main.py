326bcc4587b6ac509018e86d518314cc9102bd40
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-01-19 13:58:21 -0300

    Update main.py

--- secao6/aulas/selenium/main.py
+++ secao6/aulas/selenium/main.py
@@ -1,19 +1,40 @@
-from pathlib import Path 
+from pathlib import Path
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from time import sleep
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+
 
 ROOT_FOLDER = Path(__file__).parent
 CAMINHO_DRIVER = ROOT_FOLDER / 'drive' / 'chromedriver.exe'
 
-print(CAMINHO_DRIVER)
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)  # type: ignore
 
-chorme_options = webdriver.ChromeOptions()
-chorme_service = Service(executable_path=CAMINHO_DRIVER)
-chorme_browser = webdriver.Chrome(
-    service = chorme_service,
-    options = chorme_options,
-)
+    chrome_service = Service(
+        executable_path=str(CAMINHO_DRIVER),
+    )
 
-chorme_browser.get('https://www.youtube.com/')
-sleep(5)
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    # Example
+    # options = '--headless', '--disable-gpu',
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    browser.get('https://www.google.com')
+
+    sleep(10)
