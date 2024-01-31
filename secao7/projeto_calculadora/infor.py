96ebb338e9825658fc663838ae09ecde17f8188b
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-01-31 11:28:08 -0300

    config QLabel

---
+++ secao7/projeto_calculadora/infor.py
@@ -0,0 +1,13 @@
+from PySide6.QtWidgets import QLabel, QWidget
+from PySide6.QtCore import Qt
+from variables import *
+
+
+class Infor(QLabel):
+    def __init__(self, text: str, parent: QWidget | None = None) -> None:
+        super().__init__(text, parent)
+        self.configStyle()
+        
+    def configStyle(self):
+        self.setStyleSheet(f'font-size: {MIDIUM_FONT_SIZE}')
+        self.setAlignment(Qt.AlignmentFlag.AlignRight)
--- secao7/projeto_calculadora/main.py
+++ secao7/projeto_calculadora/main.py
@@ -4,6 +4,7 @@ from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from variables import ICON_FILE
 from display import Display
+from infor import Infor
 
 
 if __name__ == '__main__':
@@ -12,6 +13,8 @@ if __name__ == '__main__':
     display = Display()
     botao1 = QPushButton('botoa 1')
     icon = QIcon(str(ICON_FILE))
+    info = Infor('teste')
+    window.addWidgetVlayout(info, False)
     window.addWidgetVlayout(display,True)
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
