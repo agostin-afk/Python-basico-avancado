bf15d414f3dcd95fc41317b43514ed1a898abbe4
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-02-13 19:53:42 -0300

    Dbeaver

--- secao8/aulas/main.py
+++ secao8/aulas/main.py
@@ -11,7 +11,18 @@ DB_FILE = ROOT_DIR / DB_NAME
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
-# SQL
+TABLE_NAME = 'customers'
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+
+connection.commit()
 
 cursor.close()
 connection.close()
