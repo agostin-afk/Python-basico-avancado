4cf80a6bf27674a25df4e45f07768c4e47efc505
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-02-14 10:40:52 -0300

    comandos SQL para inserir dados

--- secao8/aulas/main.py
+++ secao8/aulas/main.py
@@ -13,6 +13,7 @@ cursor = connection.cursor()
 
 TABLE_NAME = 'customers'
 
+# CUIDADO: sql injection
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
@@ -20,6 +21,46 @@ cursor.execute(
     'name TEXT,'
     'weight REAL'
     ')'
+)
+
+connection.commit()
+
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight)'
+    'VALUES (NULL, "Agostinho Ferreira", 9.9 )'
+)
+
+connection.commit()
+
+# deleta a tabela:
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+
+connection.commit()
+
+# deleta os id's:
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+
+connection.commit()
+
+# melhor jeito de inserir valor:
+sql = (
+    f'INSERT INTO {TABLE_NAME}'
+    '(name, weight)'
+    'VALUES'
+    '(?, ?)'
+)
+cursor.execute(sql, ['Joana', 4])
+# print(sql)
+# passando mais de um valor:
+cursor.executemany(
+    sql,
+    [
+        ['Agosto', 13], ['Lucas', 22], ['Pedro', 17]
+    ]
 )
 
 connection.commit()
