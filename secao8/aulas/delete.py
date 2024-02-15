7d4dbeda5d9e7147418941a3e2cd76b0d7193a98
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-02-15 11:06:43 -0300

    Update delete.py

--- secao8/aulas/delete.py
+++ secao8/aulas/delete.py
@@ -6,9 +6,18 @@ connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 # Delete perigoso:
 cursor.execute(f'DELETE FORM {TABLE_NAME}')
+connection.commit()
 
 # Melhor Delete:
 cursor.execute(f'DELETE FROM sqlite_sequence WHERE name={TABLE_NAME}')
+connection.commit()
+
+# Deletar dados pelo id:
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME} '
+    'WHERE id = "1"'
+)
+connection.commit()
 
 cursor.close()
 connection.close()
