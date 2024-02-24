3bfb89a580ba9c3ac2850330dbc6ae882c3fd605
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-02-24 09:33:06 -0300

    templates e urls

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def blog(request):
+    return render(request, 'blog/home.html')
+
+def exemplo(request):
+    return render(request, 'blog/exemplo.html')
