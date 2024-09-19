da5531f5531bc99b9590dd6c59bba90aee34882f
Agostinho Ferreira <agostinho008.af45@gmail.com>
2024-09-19 17:27:46 -0300

    Create aula5.py

---
+++ expressoes_regulares/aula5.py
@@ -0,0 +1,10 @@
+import re
+
+text = """
+    <p>exemplo de paragrafo</p> <div>exemplo de div</div>
+    <p>exemplo de paragrafo2</p>
+"""
+
+print(re.findall(r'<[divp]{1,3}>.*<[\/dpiv]{1,3}>', text))
+print(re.findall(r'<[divp]{1,3}>.*?<[\/dpiv]{1,3}>', text))
+# corrigir
